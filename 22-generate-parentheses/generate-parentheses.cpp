9b1ac02eb9d76f28422c27d3c86b306fbde71551
Buni Saraswati <79354771+BuniSaraswati@users.noreply.github.com>
2024-01-06 13:21:04 +0530

    Time: 4 ms (52.09%) | Memory: 15.4 MB (41.27%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -11,18 +11,17 @@ public:
 
 private:
     void generate(vector<string>& results, string current, int openCount, int closeCount, int n) {
-        // Base case: if the length of the current string is 2n, a valid combination is found
+        // if the length of the current string is 2n, a valid combination is found
         if (current.length() == 2 * n) {
             results.push_back(current);
             return;
         }
 
-        // Add an opening parenthesis if the count of open parentheses is less than n
+    
         if (openCount < n) {
             generate(results, current + '(', openCount + 1, closeCount, n);
         }
 
-        // Add a closing parenthesis if it can match an open parenthesis
         if (closeCount < openCount) {
             generate(results, current + ')', openCount, closeCount + 1, n);
         }
